6996eaec7f0149a81e2d52dd67fe00c9b99e4e3f
Nbred <thehardyboiz1@gmail.com>
2025-07-27 17:22:22 -0500

    Hacky solution for a weird Threlte bug, for some reason, the Threlte canvas resizes on window resize but only if the window grows, it doesn't trigger a resize otherwise. This fix works by forcing a resize on the canvas on all window resizes.

--- src/components/Scene.svelte
+++ src/components/Scene.svelte
@@ -4,7 +4,19 @@
   import { isAnimationPaused } from "@/store/PropertiesPanel.svelte";
   import { FFT } from "@/store/State.svelte";
   import Chaotic from "@/scenes/Chaotic.svelte";
+  import { onMount } from "svelte";
   let rotation = $state(0);
+
+  onMount(() => {
+    const handleResize = () => {
+      const canvas = document.querySelector("canvas")!;
+      canvas.style.width = "0";
+      canvas.style.height = "0";
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
 
   useTask((delta) => {
     if (!FFT.reload || isAnimationPaused.current) return;
