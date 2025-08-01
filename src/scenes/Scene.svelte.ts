aca781daab6bcaa66a45d8dcb264f1559aa40996
Nbred <thehardyboiz1@gmail.com>
2025-08-01 18:03:56 -0500

    Revert "Implemented use Instanced mesh alternative in scene"
    This reverts commit 1eae3ec84773c6e64c1705a3e6867f8d6b7bfc44.

--- src/scenes/Scene.svelte.ts
+++ src/scenes/Scene.svelte.ts
@@ -1,4 +1,4 @@
-import { Scene, Mesh, Color, BoxGeometry, MeshBasicMaterial, Group, InstancedMesh, Object3D } from "three";
+import { Scene, Mesh, BoxGeometry, MeshBasicMaterial, Group } from "three";
 import { FFT_QUANTITY } from "@/store/DefaultValues.svelte";
 import { currentTheme, isTransitionRunning, currentScene, previousScene } from "@/store/PropertiesPanel.svelte";
 import type { MeshGroup, SceneDynamicValues, SceneProperties } from "@/types";
@@ -8,13 +8,10 @@ import type { LerpFunctions } from "@/utils/lerp";
 
 export default class CustomScene extends Scene {
   boxGeometry = new BoxGeometry(1, 1, 1);
-  boxMaterial = new MeshBasicMaterial({ color: 0xffffff });
-  instancedMesh = new InstancedMesh(this.boxGeometry, this.boxMaterial, FFT_QUANTITY);
-  dummy = new Object3D();
+  boxMaterial = new MeshBasicMaterial();
   groups: MeshGroup[] = [new Group() as MeshGroup];
   transitionTimer: number = 0;
   maxScalar: number = 256;
-  useInstancedMesh = true;
 
   initialSceneProperties = $derived(sceneMap[previousScene.current].precalculateValues());
 
@@ -26,22 +23,16 @@ export default class CustomScene extends Scene {
 
   constructor() {
     super();
-    if (this.useInstancedMesh) {
-      this.instancedMesh.frustumCulled = false;
-      this.add(this.instancedMesh);
-    }
     this.setup();
   }
 
   setup() {
-    if (!this.useInstancedMesh) {
-      this.groups.forEach((group) => {
-        for (let i = 0; i < FFT_QUANTITY; ++i) {
-          group.add(new Mesh(this.boxGeometry, this.boxMaterial.clone()));
-        }
-        this.add(group);
-      });
-    }
+    this.groups.forEach((group) => {
+      for (let i = 0; i < FFT_QUANTITY; ++i) {
+        group.add(new Mesh(this.boxGeometry, this.boxMaterial.clone()));
+      }
+      this.add(group);
+    });
     this.updateScene();
   }
 
@@ -53,35 +44,17 @@ export default class CustomScene extends Scene {
           index: i,
           sceneProperties: this.finalSceneProperties,
         });
-
-        if (this.useInstancedMesh) {
-          this.dummy.updateMatrix();
-          this.instancedMesh.setMatrixAt(i, this.dummy.matrix);
-        }
-      }
-
-      if (this.useInstancedMesh) {
-        this.instancedMesh.instanceMatrix.needsUpdate = true;
       }
     });
   }
 
   updateSceneAtIndex({ groupIndex, index, sceneProperties }: { groupIndex: number; index: number; sceneProperties: SceneProperties }) {
-    if (this.useInstancedMesh) {
-      this.dummy.position.x = sceneProperties[index].position[0];
-      this.dummy.position.y = sceneProperties[index].position[1];
-      this.dummy.position.z = sceneProperties[index].position[2];
-      this.dummy.rotation.x = sceneProperties[index].rotation[0];
-      this.dummy.rotation.y = sceneProperties[index].rotation[1];
-      this.dummy.rotation.z = sceneProperties[index].rotation[2];
-    } else {
-      this.groups[groupIndex].children[index].position.x = sceneProperties[index].position[0];
-      this.groups[groupIndex].children[index].position.y = sceneProperties[index].position[1];
-      this.groups[groupIndex].children[index].position.z = sceneProperties[index].position[2];
-      this.groups[groupIndex].children[index].rotation.x = sceneProperties[index].rotation[0];
-      this.groups[groupIndex].children[index].rotation.y = sceneProperties[index].rotation[1];
-      this.groups[groupIndex].children[index].rotation.z = sceneProperties[index].rotation[2];
-    }
+    this.groups[groupIndex].children[index].position.x = sceneProperties[index].position[0];
+    this.groups[groupIndex].children[index].position.y = sceneProperties[index].position[1];
+    this.groups[groupIndex].children[index].position.z = sceneProperties[index].position[2];
+    this.groups[groupIndex].children[index].rotation.x = sceneProperties[index].rotation[0];
+    this.groups[groupIndex].children[index].rotation.y = sceneProperties[index].rotation[1];
+    this.groups[groupIndex].children[index].rotation.z = sceneProperties[index].rotation[2];
   }
 
   useSceneTransition(timing: number, lerpType: LerpFunctions, delta: number) {
@@ -108,44 +81,21 @@ export default class CustomScene extends Scene {
 
     this.groups.forEach((group, gi) => {
       for (let i = 0; i < FFT_QUANTITY; ++i) {
-        if (!this.useInstancedMesh) {
-          if (running) {
-            this.updateSceneAtIndex({ groupIndex: gi, index: i, sceneProperties: this.finalSceneProperties });
-          }
-        } else {
+        if (running) {
           this.updateSceneAtIndex({ groupIndex: gi, index: i, sceneProperties: this.finalSceneProperties });
         }
 
         if (this.dynamicValues) {
-          if (this.useInstancedMesh) {
-            this.dummy.scale.x = this.dynamicValues(FFT[i]).scale[0];
-            this.dummy.scale.y = this.dynamicValues(FFT[i]).scale[1];
-            this.dummy.scale.z = this.dynamicValues(FFT[i]).scale[2];
-          } else {
-            group.children[i].scale.x = this.dynamicValues(FFT[i]).scale[0];
-            group.children[i].scale.y = this.dynamicValues(FFT[i]).scale[1];
-            group.children[i].scale.z = this.dynamicValues(FFT[i]).scale[2];
-          }
+          group.children[i].scale.x = this.dynamicValues(FFT[i]).scale[0];
+          group.children[i].scale.y = this.dynamicValues(FFT[i]).scale[1];
+          group.children[i].scale.z = this.dynamicValues(FFT[i]).scale[2];
         }
 
-        if (this.useInstancedMesh) {
-          this.dummy.updateMatrix();
-          this.instancedMesh.setMatrixAt(i, this.dummy.matrix);
-          this.instancedMesh.setColorAt(
-            i,
-            currentTheme.current.color.clone().lerp(currentTheme.current.transitionColor, Math.max(FFT[i] / this.maxScalar || 1))
-          );
-        } else {
-          group.children[i].material.color.lerpColors(
-            currentTheme.current.color,
-            currentTheme.current.transitionColor,
-            Math.max(FFT[i] / this.maxScalar || 1)
-          );
-        }
-      }
-      if (this.useInstancedMesh) {
-        this.instancedMesh.instanceMatrix.needsUpdate = true;
-        this.instancedMesh.instanceColor!.needsUpdate = true;
+        group.children[i].material.color.lerpColors(
+          currentTheme.current.color,
+          currentTheme.current.transitionColor,
+          Math.max(FFT[i] / this.maxScalar || 1)
+        );
       }
     });
 
